35b83b63e5920ec1dbf9b7539a1aa6a81c3ad7b0
Shane <shiboxuan0131@icloud.com>
2021-05-24 18:23:39 +0100

    Initial Commit

---
+++ LearningGH-shane/LearningGH_shaneApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGH_shaneApp.swift
+//  LearningGH-shane
+//
+//  Created by 石帛轩 on 24/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct LearningGH_shaneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
